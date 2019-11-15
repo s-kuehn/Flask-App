1e1b0f112bfdbe3a973f1a07f5dcc75180b731c2
Ecommerce Department <paneltown@Ecommerces-MacBook-Pro.local>
2019-11-15 14:13:33 -0500

    messing with routes

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return 'home page'
+
+@app.route('/contact')
+def contact():
+	return 'contact page'
+
+@app.route('/about')
+def about():
+	return 'about page'
