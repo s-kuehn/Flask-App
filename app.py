c7cebdf607d4281638fed445a8264a09d38460c3
Ecommerce Department <paneltown@Ecommerces-MacBook-Pro.local>
2019-11-15 14:52:00 -0500

    added calculator fields

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'home page'
+	return render_template('index.html')
 
 @app.route('/contact')
 def contact():
