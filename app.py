6defee671abde13409942f68c959d17870aa7a9d
Ecommerce Department <paneltown@Ecommerces-MacBook-Pro.local>
2019-11-16 19:22:48 -0500

    Added basic calculator funtionality

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+	if request.method == 'POST':
+		cost = request.form['cost']
+		revenue = request.form['revenue']
+		print(cost)
+
+		sum = float(cost) + float(revenue)
+		return render_template('index.html', sum=sum)
+
 @app.route('/contact')
 def contact():
 	return 'contact page'
